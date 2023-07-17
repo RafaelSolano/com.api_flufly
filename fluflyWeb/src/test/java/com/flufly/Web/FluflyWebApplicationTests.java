f5b0cf030c717ba21ddeff37cf83f475b796c6c8
Rafael Solano <rafaelsolanol007@gmail.com>
2023-07-17 09:42:22 -0500

    Se agrega el proyecto primer commit

---
+++ fluflyWeb/src/test/java/com/flufly/Web/FluflyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flufly.Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluflyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
